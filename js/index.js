5ebd0ffe8f536fb948e429096e8628475f258b63
AdrianHDev <adrianhensondev@gmail.com>
2021-05-04 13:17:18 -0400

    Add eventListeners to navLinks, update css to have transition-speed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+let navLinks = document.querySelectorAll('.nav-link');
+let navBar = document.querySelector('.nav');
+
+navLinks.forEach((navLink) => {
+
+    /* Add event listeners */
+    navLink.addEventListener('mouseover', () => {
+        navLink.style.color = 'red';
+        navBar.style.background = 'gray';
+    })
+    navLink.addEventListener('mouseout', () => {
+        navLink.style.color = '#212529';
+        navBar.style.background = '';
+    })
+});
+console.log('yes');
